7cacb38ce9812c46c5fb13a9a0a46fbbac26ae9e
animeheacen <pavanvallu2@gmail.com>
2025-07-01 20:58:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Recommended
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "0")
-APP_ID = int(os.environ.get("APP_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "0")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "TG_BOT_TOKEN")
+APP_ID = int(os.environ.get("APP_ID", "APP_ID"))
+API_HASH = os.environ.get("API_HASH", "API_HASH")
 
 ##---------------------------------------------------------------------------------------------------
 
 #Main 
-OWNER_ID = int(os.environ.get("OWNER_ID", "5090651635"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "OWNER_ID"))
 PORT = os.environ.get("PORT", "8010")
 
 ##---------------------------------------------------------------------------------------------------
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "0")
+DB_URI = os.environ.get("DATABASE_URL", "DATABASE_URL")
 DB_NAME = os.environ.get("DATABASE_NAME", "links-sharing")
 
 ##---------------------------------------------------------------------------------------------------
@@ -27,7 +27,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "") #No Need keep it blank
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5090651635").split()):
+    for x in (os.environ.get("ADMINS", "7825330156").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
